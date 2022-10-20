01e06c5cf79dcfef8c588beeb2567f1d4d65014e
simPRODan <56947302+simPRODan@users.noreply.github.com>
2022-10-20 15:01:06 +1000

    Updated TCPDF to round values to 5dp
    Rounded to 5dp as the floating point numbers were not matching even though they should have been. Which was meaning that some data which should have been printed, was not printed.

--- tcpdf.php
+++ tcpdf.php
@@ -6382,7 +6382,7 @@ class TCPDF {
 		$pc = 0; // previous character
 		// for each character
 		while ($i < $nb) {
-			if (($maxh > 0) AND ($this->y > $maxy) ) {
+			if (($maxh > 0) AND (number_format($this->y, 5) > number_format($maxy, 5)) ) {
 				break;
 			}
 			//Get the current character
